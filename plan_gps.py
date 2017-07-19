9ee290ba62927a24b132590475a2a97c6568a0ea
skiff <skiffc@gmail.com>
2017-07-20 04:45:13 +0800

    go much straight in plan gps

--- plan_gps.py
+++ plan_gps.py
@@ -4,6 +4,7 @@ import sys
 import os
 import re
 import random
+from math import sqrt
 
 buf = ""
 pos = []
@@ -18,13 +19,16 @@ for p in open( sys.argv[1] ):
     if not pos:
         pos = tpos
     while True: 
-        dis2 = ( tpos[0] - pos[0] ) * ( tpos[0] - pos[0] ) + ( tpos[1] - pos[1] ) * ( tpos[1] - pos[1] )
+        dis2 = sqrt( ( tpos[0] - pos[0] ) * ( tpos[0] - pos[0] ) + ( tpos[1] - pos[1] ) * ( tpos[1] - pos[1] ) )
         print dis2
-        if dis2 < circle * circle: 
+        if dis2 < circle: 
             break
-        r = max( random.random(), 0.3 )
-        pos[0] += r * ( tpos[0] - pos[0] ) * speed / abs( tpos[0] - pos[0] )  
-        pos[1] += r * ( tpos[1] - pos[1] ) * speed / abs( tpos[1] - pos[1] )
+        if dis2 > speed * 3:
+            r = max( random.random() * 0.9, 0.6 )
+        else:
+            r = max( random.random() * 0.7, 0.4 )
+        pos[0] += r * speed * ( tpos[0] - pos[0] ) / dis2 
+        pos[1] += r * speed * ( tpos[1] - pos[1] ) / dis2 
         print pos
         buf += '<wpt lat="%f" lon="%f"></wpt>\n' % ( pos[0], pos[1] )
 
